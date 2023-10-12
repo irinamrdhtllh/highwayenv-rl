b928fc736abe85394e0524c5174703751a2aff54
Irina M. S <irinamardhatillah@gmail.com>
2023-10-12 15:54:43 +0700

    Add code to test the gym environment

---
+++ test_code.py
@@ -0,0 +1,10 @@
+import gymnasium as gym
+import matplotlib.pyplot as plt
+
+env = gym.make("highway-v0", render_mode="rgb_array")
+env.reset()
+
+for _ in range(5):
+    action = env.action_type.actions_indexes["IDLE"]
+    obs, reward, done, truncated, info = env.step(action)
+    env.render()
